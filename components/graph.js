42e95d92270135f4d08936ef5789e5d46e6f00e2
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2023-01-19 23:22:48 +0330

    Add files via upload

--- components/graph.js
+++ components/graph.js
@@ -1,9 +1,28 @@
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 import styles from "./graph.module.css";
 import axios from "axios";
 // import TradingViewWidget, { Themes } from "react-tradingview-widget";
-import AdvancedChart from "react-tradingview-embed";
+// import {AdvancedChart} from "react-tradingview-embed";
+// import {TradeViewChart} from "react-crypto-chart";
 // import { TradeViewChart } from "react-crypto-chart/lib/esm/utils/types";
+const TradingViewWidget = dynamic(() => {
+    return import("react-tradingview-widget");
+    },
+    {ssr: false}
+)
+
+const {AdvancedChart} = dynamic(() => {
+    return import("react-tradingview-embed");
+    },
+    {ssr: false}
+)
+
+// const TradeViewChart = dynamic(() => {
+//     return import("react-crypto-chart");
+//     },
+//     {ssr: false}
+// )
 
 const Graph = () => {
 
@@ -37,17 +56,15 @@ const Graph = () => {
 
             {/* <p>Graph</p> */}
 
-            {/* <TradingViewWidget 
-                symbol="NASDAQ:AAPL"
-                theme={Themes.Dark}
+            <TradingViewWidget 
+                symbol="BITSTAMP:BTCUSD"
+                // theme={Themes.Dark}
                 lacale="fr"
                 autosize
-            /> */}
+            />
 
 
-            <div className={styles.chart}>
-                <AdvancedChart widgetProps={{"theme": "dark"}} />
-            </div>
+            {/* <AdvancedChart widgetProps={{"theme": "dark"}} /> */}
 
             {/* <TradeViewChart pair="BTCBUSD" /> */}
 
@@ -57,3 +74,63 @@ const Graph = () => {
 }
 
 export default Graph
+
+// TradingViewWidget.js
+
+// import React, { useEffect, useRef } from 'react';
+
+// let tvScriptLoadingPromise;
+
+// export default function TradingViewWidget() {
+//   const onLoadScriptRef = useRef();
+
+//   useEffect(
+//     () => {
+//       onLoadScriptRef.current = createWidget;
+
+//       if (!tvScriptLoadingPromise) {
+//         tvScriptLoadingPromise = new Promise((resolve) => {
+//           const script = document.createElement('script');
+//           script.id = 'tradingview-widget-loading-script';
+//           script.src = 'https://s3.tradingview.com/tv.js';
+//           script.type = 'text/javascript';
+//           script.onload = resolve;
+
+//           document.head.appendChild(script);
+//         });
+//       }
+
+//       tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+//       return () => onLoadScriptRef.current = null;
+
+//       function createWidget() {
+//         if (document.getElementById('tradingview_04732') && 'TradingView' in window) {
+//           new window.TradingView.widget({
+//             autosize: true,
+//             symbol: "BITSTAMP:BTCUSD",
+//             interval: "D",
+//             timezone: "Etc/UTC",
+//             theme: "dark",
+//             style: "1",
+//             locale: "en",
+//             toolbar_bg: "#f1f3f6",
+//             enable_publishing: false,
+//             allow_symbol_change: true,
+//             container_id: "tradingview_04732"
+//           });
+//         }
+//       }
+//     },
+//     []
+//   );
+
+//   return (
+//     <div className='tradingview-widget-container'>
+//       <div id='tradingview_04732' />
+//       <div className="tradingview-widget-copyright">
+//         <a href="https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP" rel="noopener" target="_blank"><span className="blue-text">Bitcoin chart</span></a> by TradingView
+//       </div>
+//     </div>
+//   );
+// }
