4dcd4bbddb1689fd69a1ae528109b628140ad36f
AliakbarNateghi <88666781+AliakbarNateghi@users.noreply.github.com>
2023-01-22 07:11:40 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://cex.io/:path*",
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
